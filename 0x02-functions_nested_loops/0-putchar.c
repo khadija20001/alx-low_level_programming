2b2e84475511290ae2edef1fa96229e98b2b373c
khadija20001 <khadijaelgeuddari06@gmail.com>
2023-02-21 09:07:41 -0800

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,15 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * main - this program print _putchar
  * Return:0
  */
 int main(void)
 {
-	printf("_putchar\n");
+	char c[] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
+	int i;
+
+	for (i = 0; i < 9; i++)
+		_putchar(c[i]);
+	_putchar('\n');
 	return (0);
 }
