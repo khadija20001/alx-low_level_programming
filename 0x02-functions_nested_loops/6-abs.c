2ec8a3662f53f5b0cbf958809115f8dd4e9f04fa
khadija20001 <khadijaelgeuddari06@gmail.com>
2023-02-22 02:58:57 -0800

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *_abs-this function that computes the absolute value
+ *@n:the parameter
+ *Return: n or -n
+ */
+int _abs(int n)
+{
+	if (n <= 0)
+		return (-n);
+	else
+		return (n);
+}
