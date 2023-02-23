bc12267ba4e6ff934b578c2263f929af6b9b85eb
khadija20001 <khadijaelgeuddari06@gmail.com>
2023-02-23 10:36:31 -0800

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,21 +12,24 @@ int main(void)
 	{
 		if ((n % 3) == 0 && (n % 5) != 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else if ((n % 5) == 0 && (n % 3) != 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else if ((n % 3) == 0 && (n % 5) == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else
 		{
 			printf("%d ", n);
 		}
+		if (n != 100)
+			putchar(' ');
 		n++;
 	}
+	putchar('\n');
 	return (0);
 }
