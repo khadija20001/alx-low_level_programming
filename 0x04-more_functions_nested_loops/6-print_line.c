ad9537d9cf62c32121d1d393a7c27af680099a81
khadija20001 <khadijaelgeuddari06@gmail.com>
2023-02-23 06:56:55 -0800

    lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_line-print line
+ * @n:input
+ */
+void print_line(int n)
+{
+int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i <= n; i++)
+			_putchar('-');
+	_putchar('\n');
+	}
+}
