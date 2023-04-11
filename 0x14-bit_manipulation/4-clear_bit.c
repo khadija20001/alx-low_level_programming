604ee7430c6ba09ad7f563c783102efc454612b2
khadija20001 <khadijaelgeuddari06@gmail.com>
2023-04-11 15:57:16 -0700

    clear

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -9,9 +9,7 @@ int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
-	unsigned long int mask = 1 << index;
 
-	*n &= ~mask;
-
+	*n &= ~(1 << index);
 	return (1);
 }
